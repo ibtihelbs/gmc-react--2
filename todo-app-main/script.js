24390d007da6ad8c94a5f05ed6ba5e21608494c8
ibtihelbs <ibtihel.bensalah@outlook.fr>
2024-10-05 13:40:43 +0200

    edit

--- todo-app-main/script.js
+++ todo-app-main/script.js
@@ -2,6 +2,17 @@ const form = document.querySelector("form");
 const addTaskIn = document.querySelector("#add");
 const center = document.querySelector(".center");
 const tasksWrap = document.querySelector("#tasks-wrap");
+function handleComplete() {
+  const checkbox = document.querySelectorAll(".state");
+  const singleTasks = document.querySelectorAll("p");
+
+  checkbox.forEach(function (v, i) {
+    v.addEventListener("change", function (e) {
+      singleTasks[i].classList.toggle("lined");
+    });
+  });
+}
+handleComplete();
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   const tasks = `<div class="single-task flex between">
@@ -15,7 +26,7 @@ form.addEventListener("submit", function (e) {
                 <img src="./images/icon-cross.svg" alt="icon-cross" />
               </button>
             </div>`;
-  console.log(typeof tasks);
   tasksWrap.innerHTML += tasks;
   addTaskIn.value = "";
+  handleComplete();
 });
