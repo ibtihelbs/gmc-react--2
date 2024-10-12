db86be5463db1fea7ea80c7d2ef00d859e3fbc83
ibtihelbs <ibtihel.bensalah@outlook.fr>
2024-10-12 11:07:20 +0200

    es6 - display + edit

--- todo-app-main/script.js
+++ todo-app-main/script.js
@@ -2,42 +2,42 @@ const form = document.querySelector("form");
 const addTaskIn = document.querySelector("#add");
 const center = document.querySelector(".center");
 const tasksWrap = document.querySelector("#tasks-wrap");
-function handleComplete() {
-  const checkbox = document.querySelectorAll(".state");
-  const singleTasks = document.querySelectorAll("p");
-
-  checkbox.forEach(function (v, i) {
-    v.addEventListener("change", function () {
-      singleTasks[i].classList.toggle("lined");
-    });
-  });
-}
-function handleDelete() {
-  const toDelete = document.querySelectorAll(".delete");
-  const singleTask = document.querySelectorAll(".single-task");
-  toDelete.forEach(function (v, i) {
-    v.addEventListener("click", function () {
-      singleTask[i].remove();
-    });
-  });
-}
-handleComplete();
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  const tasks = `<div class="single-task flex between">
+let tasksArr = [];
+const displayTasks = () => {
+  const display = tasksArr
+    .map((v) => {
+      const { task, done } = v;
+      return `<div class="single-task flex between">
               <div class="flex">
                 <form class="checked">
-                  <input type="checkbox" class="state" />
+                  <input type="checkbox" ${
+                    done ? "checked" : ""
+                  } class="state" />
                 </form>
-                <p>${addTaskIn.value} </p>
+                <p ${done ? "class='lined'" : ""} >${task} </p>
               </div>
               <button class="delete">
                 <img src="./images/icon-cross.svg" alt="icon-cross" />
               </button>
             </div>`;
-
-  tasksWrap.innerHTML += tasks;
+    })
+    .join("");
+  tasksWrap.innerHTML = display;
+  handleCompleted();
+};
+const handleCompleted = () => {
+  const checkbox = document.querySelectorAll(".state");
+  checkbox.forEach((v, i) => {
+    console.log(v);
+    v.addEventListener("change", (e) => {
+      tasksArr[i].done = e.target.checked;
+      displayTasks();
+    });
+  });
+};
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  tasksArr = [...tasksArr, { done: false, task: addTaskIn.value }];
+  displayTasks();
   addTaskIn.value = "";
-  handleComplete();
-  handleDelete();
 });
